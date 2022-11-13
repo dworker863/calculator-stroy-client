3468f95df79318330dc603738d65edcf3078a3ff
dworker <dworker863@gmail.com>
2022-11-13 18:53:52 +0600

    Added action, thunk creator and api for post, update service

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,12 +1,37 @@
 import axios from 'axios';
+import { IService } from '../commonInterfaces/IServices';
 
 export const instance = axios.create({
   baseURL: 'http://localhost:8000/',
 });
 
-export const fetchServices = (): Promise<any> => {
+export const fetchServices = (): Promise<IService[]> => {
   return instance
     .get('services')
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e.message);
+      return e.message;
+    });
+};
+
+export const postService = (service: IService): Promise<IService> => {
+  return instance
+    .post('services', service)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e.message);
+      return e.message;
+    });
+};
+
+export const patchService = (fields: any) => {
+  return instance
+    .patch('services', fields)
     .then((res) => {
       return res.data;
     })
--- src/app/redux/reducers/servicesReducer.ts
+++ src/app/redux/reducers/servicesReducer.ts
@@ -1,14 +1,20 @@
-import { fetchServices } from './../../api/api';
+import { fetchServices, postService, patchService } from './../../api/api';
 import { AppThunk } from './../store';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IService } from '../../commonInterfaces/IServices';
 
+type TLoading = 'pending' | 'succeeded' | 'failed';
+
 export type TServiceState = {
+  loading: TLoading;
   services: IService[];
+  errorMessage: string;
 };
 
 export const initialState: TServiceState = {
   services: [],
+  loading: 'succeeded',
+  errorMessage: '',
 };
 
 const servicesSlice = createSlice({
@@ -19,14 +25,44 @@ const servicesSlice = createSlice({
       state.services = action.payload;
       return state;
     },
+    setLoading: (state, action: PayloadAction<TLoading>) => {
+      state.loading = action.payload;
+      return state;
+    },
+    setError: (state, action: PayloadAction<string>) => {
+      state.errorMessage = action.payload;
+      return state;
+    },
   },
 });
 
 export const getServices = (): AppThunk => async (dispatch) => {
+  dispatch(setLoading('pending'));
   const services = await fetchServices();
+
+  if (typeof services === 'string') {
+    dispatch(setLoading('failed'));
+    dispatch(setError(services));
+  }
+
+  dispatch(setLoading('succeeded'));
   dispatch(setServices(services));
 };
 
-export const { setServices } = servicesSlice.actions;
+export const addService =
+  (service: IService): AppThunk =>
+  async (dispatch) => {
+    await postService(service);
+    dispatch(getServices());
+  };
+
+export const changeService =
+  (fields: any): AppThunk =>
+  async (dispatch) => {
+    await patchService(fields);
+    dispatch(getServices());
+  };
+
+export const { setServices, setLoading, setError } = servicesSlice.actions;
 
 export const servicesReducer = servicesSlice.reducer;
